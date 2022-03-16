37257278e7d347a9d4835a3aca81fcc2dc89e0af
CharlieYang <CharlieYang@wistronits.com>
2022-03-16 16:05:45 +0800

    V1.0.0 將卡拉OK的Lyc改為一串

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace LRCtools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.功能ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.儲存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.單字恢復長串ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.離開ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.關於ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(799, 448);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtSource);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(791, 415);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "來源";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // txtSource
+            // 
+            this.txtSource.AllowDrop = true;
+            this.txtSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSource.Font = new System.Drawing.Font("Source Code Pro Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSource.Location = new System.Drawing.Point(3, 3);
+            this.txtSource.Margin = new System.Windows.Forms.Padding(5);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSource.Size = new System.Drawing.Size(785, 409);
+            this.txtSource.TabIndex = 1;
+            this.txtSource.Text = resources.GetString("txtSource.Text");
+            this.txtSource.WordWrap = false;
+            this.txtSource.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtSource_DragDrop);
+            this.txtSource.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtSource_DragEnter);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtTarget);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(791, 415);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "結果";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.AllowDrop = true;
+            this.txtTarget.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtTarget.Font = new System.Drawing.Font("Source Code Pro Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTarget.Location = new System.Drawing.Point(3, 3);
+            this.txtTarget.Margin = new System.Windows.Forms.Padding(5);
+            this.txtTarget.Multiline = true;
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.ReadOnly = true;
+            this.txtTarget.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtTarget.Size = new System.Drawing.Size(785, 409);
+            this.txtTarget.TabIndex = 3;
+            this.txtTarget.TextChanged += new System.EventHandler(this.txtTarget_TextChanged);
+            this.txtTarget.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtSource_DragDrop);
+            this.txtTarget.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtSource_DragEnter);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.功能ToolStripMenuItem,
+            this.關於ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(799, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 功能ToolStripMenuItem
+            // 
+            this.功能ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.儲存ToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.單字恢復長串ToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.離開ToolStripMenuItem});
+            this.功能ToolStripMenuItem.Name = "功能ToolStripMenuItem";
+            this.功能ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.功能ToolStripMenuItem.Text = "功能";
+            // 
+            // 儲存ToolStripMenuItem
+            // 
+            this.儲存ToolStripMenuItem.Name = "儲存ToolStripMenuItem";
+            this.儲存ToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F10;
+            this.儲存ToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.儲存ToolStripMenuItem.Text = "儲存";
+            this.儲存ToolStripMenuItem.Click += new System.EventHandler(this.儲存ToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(163, 6);
+            // 
+            // 單字恢復長串ToolStripMenuItem
+            // 
+            this.單字恢復長串ToolStripMenuItem.Name = "單字恢復長串ToolStripMenuItem";
+            this.單字恢復長串ToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.單字恢復長串ToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.單字恢復長串ToolStripMenuItem.Text = "單字恢復長串";
+            this.單字恢復長串ToolStripMenuItem.Click += new System.EventHandler(this.單字恢復長串ToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(163, 6);
+            // 
+            // 離開ToolStripMenuItem
+            // 
+            this.離開ToolStripMenuItem.Name = "離開ToolStripMenuItem";
+            this.離開ToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.離開ToolStripMenuItem.Text = "離開";
+            this.離開ToolStripMenuItem.Click += new System.EventHandler(this.離開ToolStripMenuItem_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "lrc";
+            this.saveFileDialog1.Filter = "LRC|*.lrc";
+            this.saveFileDialog1.RestoreDirectory = true;
+            // 
+            // 關於ToolStripMenuItem
+            // 
+            this.關於ToolStripMenuItem.Name = "關於ToolStripMenuItem";
+            this.關於ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.關於ToolStripMenuItem.Text = "關於";
+            this.關於ToolStripMenuItem.Click += new System.EventHandler(this.關於ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 472);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LRC tools";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 功能ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 單字恢復長串ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 儲存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 離開ToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem 關於ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LRCtools
+{
+    public partial class Form1 : Form
+    {
+        LRCdata ld;
+            
+        public Form1()
+        {
+            InitializeComponent();
+            newObject();
+        }
+
+        private void 離開ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        private void 儲存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ld.lstLRC.Count > 0)
+            {
+                saveFileDialog1.FileName = ld.ar != null && ld.ti != null && ld.ar.Length > 0 && ld.ti.Length > 0 ?
+                    string.Format("{0} - {1}.lrc", ld.ar, ld.ti) : "";
+                if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    System.IO.File.WriteAllText(
+                        saveFileDialog1.FileName,
+                        txtTarget.Text,
+                        Encoding.Unicode
+                        );
+            }
+        }
+
+        private void 單字恢復長串ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txtSource.Text.Trim().Length > 0)
+                txtTarget.Text = funCharToLongString(txtSource.Text.Trim());
+            tabControl1.SelectTab(tabControl1.TabCount - 1);
+        }
+
+        private void 關於ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new AboutBox1().Show();
+        }
+
+        #region txtSource
+        private void txtSource_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+                txtSource.Cursor = System.Windows.Forms.Cursors.Arrow;  //指定鼠標形狀（更好看）  
+            }
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void txtSource_DragDrop(object sender, DragEventArgs e)
+        {
+            string strFile = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            txtSource.Text = System.IO.File.ReadAllText(strFile, Encoding.Unicode);
+            txtSource.Cursor = System.Windows.Forms.Cursors.IBeam; //還原鼠標形狀
+            tabControl1.SelectTab(0);
+
+            單字恢復長串ToolStripMenuItem_Click(sender, e);
+            if (ld.bJone)
+                儲存ToolStripMenuItem_Click(sender, e);
+        }
+        #endregion txtSource
+
+        #region txtTarget
+        private void txtTarget_TextChanged(object sender, EventArgs e)
+        {
+            txtTarget.SelectionLength = 0;
+        }
+        #endregion txtTarget
+
+        #region 自定功能
+        private void newObject()
+        {
+            ld = new LRCdata();
+        }
+
+        private string funCharToLongString(string strData)
+        {
+            newObject();
+            foreach (string s in mySplit(strData))
+                ld.Add(s);
+            return ld.Output();
+        }
+
+        private string[] mySplit(string strData, string strSplit = "\n,\r\n")
+        {
+            return strData.Split(strSplit.Split(','), StringSplitOptions.RemoveEmptyEntries);
+        }
+        #endregion 自定功能
+
+    }
+
+    public class LRCdata
+    {
+        public string ar { get; set; }
+        public string ti { get; set; }
+        public string by { get; set; }
+
+        public bool bJone { get; set; }
+
+        public List<LineData> lstLRC = new List<LineData>();
+
+        public void Add(string strData)
+        {
+            //[00:02.69]你[00:02.89]是[00:03.04]不[00:03.24]是[00:03.67]像[00:03.95]我[00:04.30]在[00:04.82]太[00:05.05]陽[00:05.41]下[00:05.73]低[00:06.16]頭[00:07.06]
+            //[00:25.33][01:39.04][03:14.58]也許世界就這樣
+            if (strData.Length > 0)
+            {
+                LineData ld = new LineData();
+                string[] aryD = strData.Split('[');
+
+                //尋找整串是不是都只有一個字, 數量超過3個就是可以儲存
+                if (bJone == false)
+                    bJone = aryD
+                        .Where(x => x.IndexOf(']') != -1)
+                        .Select(x => x.Split(']')[1])
+                        .Where(x => x.Length == 1)
+                        .Count() > 3;
+
+                for (int i = 0; i < aryD.Length; i++)
+                {
+                    string d = aryD[i];
+                    if (d.Length == 0)
+                        continue;
+
+                    if (d.StartsWith("ar:"))
+                        ar = d.Replace("]", "").Substring(3);
+                    else if (d.StartsWith("ti:"))
+                        ti = d.Replace("]", "").Substring(3);
+                    else if (d.StartsWith("by:"))
+                        by = d.Replace("]", "").Substring(3);
+                    else
+                    {
+                        if (ld.lstTime == null)
+                            ld.lstTime = new List<string>();
+
+                        string[] aryD1 = d.Split(']');
+                        if (aryD1.Length > 1)
+                        {
+                            if (ld.lstTime.Count == 0 || (aryD1[1].Length == 0 && i != aryD.Length - 1))
+                                ld.lstTime.Add(aryD1[0]);
+                            else if (aryD1[1].Length > 0 && i == aryD.Length - 1)
+                                ld.lstTime.Add(aryD1[0]);
+                            ld.Content += aryD1[1];
+                        }
+                    }
+                }
+                if (ld.lstTime != null)
+                    lstLRC.Add(ld);
+            }
+        }
+
+        public string Output()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (lstLRC.Count > 0)
+            {
+                if (ar != null)
+                    sb.AppendLine(string.Format("[ar:{0}]", ar));
+                if (ti != null)
+                    sb.AppendLine(string.Format("[ti:{0}]", ti));
+                if (by != null)
+                    sb.AppendLine(string.Format("[by:{0}]", by));
+
+                foreach(LineData ld in lstLRC)
+                    sb.AppendLine(string.Format("{0}{1}", string.Join("", ld.lstTime.Select(x => "[" + x + "]").ToArray()), ld.Content));
+            }
+            return sb.ToString();
+        }
+    }
+
+    public class LineData
+    {
+        public List<string> lstTime { get; set; }
+        public string Content { get; set; }
+    }
+}
